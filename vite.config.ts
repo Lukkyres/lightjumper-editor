0b5242ee4659a06b7e07beee0278843c7983920a
Lukas Riesel <lukasriesel@example.com>
2025-05-28 07:13:42 +0200

    Initial commit of Lightjumper Editor

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin'
     }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
   }
 })
